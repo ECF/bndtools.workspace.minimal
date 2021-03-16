579b3623888ba48e1ae02782f36397a239321f9d
slewis <slewis@composent.com>
2021-03-15 18:25:44 -0700

    Added name=HelloServiceImpl to component

--- com.example.hello.impl/src/main/java/com/example/hello/impl/HelloServiceImpl.java
+++ com.example.hello.impl/src/main/java/com/example/hello/impl/HelloServiceImpl.java
@@ -4,7 +4,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.propertytypes.ExportedService;
 import com.example.hello.api.HelloService;
 
-@Component(enabled=false)
+@Component(enabled=false,name="HelloServiceImpl")
 @ExportedService(service_exported_interfaces = HelloService.class, service_exported_configs = "ecf.generic.server")
 public class HelloServiceImpl implements HelloService {
 
