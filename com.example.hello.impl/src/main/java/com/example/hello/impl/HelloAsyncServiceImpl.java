ec000154b8c4828070937a4c3a4e45bfb7c3ff97
slewis <slewis@composent.com>
2021-03-17 12:17:50 -0700

    Removed unnecessary comments

--- com.example.hello.impl/src/main/java/com/example/hello/impl/HelloAsyncServiceImpl.java
+++ com.example.hello.impl/src/main/java/com/example/hello/impl/HelloAsyncServiceImpl.java
@@ -24,15 +24,3 @@ public class HelloAsyncServiceImpl implements HelloAsyncService {
 	}
 
 }
-//Examples for use of other distribution providers.  
-//For complete list of available providers and their associated OSGi service exported config 
-//value see: https://wiki.eclipse.org/Distribution_Providers
-//
-//r-osgi: ecf.r_osgi.peer documentation: https://wiki.eclipse.org/Distribution_Providers#r-OSGi_Provider
-//@ExportedService(service_exported_interfaces = ExampleRemoteService.class, service_exported_configs = "ecf.r_osgi.peer")
-//Hazelcast: ecf.jms.hazelcast.manager full documentation: https://wiki.eclipse.org/Distribution_Providers#Hazelcast_Provider
-//@ExportedService(service_exported_interfaces = ExampleRemoteService.class, service_exported_configs = "ecf.jms.hazelcast.manager")
-//To use other provider replace the @ExportedService line above ExampleRemoteServiceImpl with the
-//desired distribution provider service exported config value.
-//For a list of ECF distribution providers and their associated exported config id see:  
-//https://wiki.eclipse.org/Distribution_Providers
